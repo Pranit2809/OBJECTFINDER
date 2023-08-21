76e6856a16eba36d7f0373366afed97beac1b75e
Pranit2809 <95509557+Pranit2809@users.noreply.github.com>
2023-08-21 11:26:12 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+status = ""
+objects=[]
+
+function setup(){
+    canvas = createCanvas(480,380)
+    canvas.center()
+
+    video = createCapture(VIDEO)
+    video.size(480, 380);
+    video.hide()
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    obj_input = document.getElementById("input").value
+}
+
+function modelLoaded(){
+    console.log("model loaded!!!")
+    status = true;
+}
+
+function draw(){
+    image(video, 0, 0, 480, 380)
+    if(status != ""){
+        objectDetector.detect(video, gotResult);
+        for (i=0; i < objects.length; i++) {
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height,)
+
+            if(objects[i].label == obj_input){
+                video.stop();
+                document.getElementById("Object_Found").innerHTML = objects[i].label + " Found"
+            }
+        }
+    }
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+ }
